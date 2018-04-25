156451ea32e32c849d2ef99cc0bced7044faecde
Hushino <frostshock@outlook.com>
2018-04-24 23:39:38 -0300

    fixes

--- mercado/resources/views/welcome.blade.php
+++ mercado/resources/views/welcome.blade.php
@@ -7,8 +7,6 @@
         <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
         <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a>
         <a class="btn btn-primary btn-lg" href="/register" role="button">Register</a>
-
-
     </div>
 @endsection
 
